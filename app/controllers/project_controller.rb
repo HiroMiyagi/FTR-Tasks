282b04e5b7af530885fc5dcade459a4e3333d29b
HiroMiyagi <isniilevsata_2012@yahoo.co.jp>
2021-06-10 01:23:48 +0900

    ルーティングとコントローラーの設定

---
+++ app/controllers/project_controller.rb
@@ -0,0 +1,19 @@
+class ProjectController < ApplicationController
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project(project_params)
+    if @project.save
+      redirect_to :root
+    else
+      render action: :new
+    end
+  end
+  
+  private
+  def project_params
+    params.require(:project).permit(:name, :job_num, :order_amount, :delivery_date).merge(create_user_id: current_user.id)
+  end
+end
